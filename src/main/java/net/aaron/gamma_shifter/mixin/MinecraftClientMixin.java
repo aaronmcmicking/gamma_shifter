e3abbd590016e6f7dbf977fa4b345c3f32a6a65d
Aaron McMicking <aaronmcmicking@outlook.com>
2023-04-29 04:39:39 -0700

    Changed mixin injection point to reduced irrelevant calls to MinecraftClientMixin.java. Needs documentation, testing to confirm readiness for release of 0.0.2.

--- src/main/java/net/aaron/gamma_shifter/mixin/MinecraftClientMixin.java
+++ src/main/java/net/aaron/gamma_shifter/mixin/MinecraftClientMixin.java
@@ -4,10 +4,8 @@ import net.aaron.gamma_shifter.GammaShifter;
 import net.aaron.gamma_shifter.GammaShifterClient;
 import net.aaron.gamma_shifter.initGammaHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,38 +14,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  * Spongepowered mixin signals to {@link initGammaHelper} to set the gamma file read from options.txt when the title screen loads for the first time.
  */
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin {
     /**
+     * NEEDS REVISION
      * Signals to set the gamma value read from options.txt on initialization when the title screen loads for the
      * first time.
-     * @see initGammaHelper
+     *
      * @param screen The Screen to change to (nullable).
      * @param ci     CallbackInfo to return.
+     * @see initGammaHelper
      */
     @Inject(method = "setScreen", at = @At("HEAD"))
     public void setScreenGammaInject(Screen screen, CallbackInfo ci) {
-        if (!GammaShifterClient.gammaHelper.alreadyDone()) {
+        if (!GammaShifterClient.gammaHelper.alreadyDone()) { // if setting the initial value from options.txt
             GammaShifterClient.gammaHelper.setInitialGamma();
-        }
-//        else{
-//            if(screen.equals(new GameMenuScreen()));
-//        }
-    }
-
-    @Shadow
-    public Screen currentScreen;
-
-    /**
-     * Saves the game options to options.txt whenever the pause menu is opened. The player must not be null (ie. must be
-     * in a singleplayer/multiplayer world) to avoid running continuously on other menus.
-     * @param ci CallbackInfo to be returned.
-     */
-    @Inject(method = "openPauseMenu", at = @At("HEAD"))
-    public void saveOptionsOnPauseMenu(boolean pause, CallbackInfo ci){
-        MinecraftClient mc = MinecraftClient.getInstance();
-        if(mc != null && mc.options != null && mc.player != null && this.currentScreen == null){
-            mc.options.write();
-            GammaShifter.LOGGER.info("Saved options");
+        } else {
+            // if saving options while player is playing
+            if (screen != null && MinecraftClient.getInstance() != null && MinecraftClient.getInstance().player != null) {
+                MinecraftClient.getInstance().options.write();
+                GammaShifter.LOGGER.info(("Saved options"));
+            }
         }
     }
 }
